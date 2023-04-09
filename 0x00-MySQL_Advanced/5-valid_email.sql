ce1cf7ecd40aa9c9782ce1bb2f252ccb2e91c32d
joethesaint <joebamisaye068@gmail.com>
2023-04-09 00:06:23 -0400

    add task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Reset the attribute valid_email 
+-- Only when the email is changed
+
+DELIMITER $$ ;
+CREATE TRIGGER email_trim
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+  IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+  END IF;
+END $$
+DELIMITER ;
