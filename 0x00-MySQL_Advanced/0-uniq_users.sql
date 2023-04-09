c3b5bf66011ac5b0013ffe51f1373c07a16d5526
joethesaint <joebamisaye068@gmail.com>
2023-04-08 23:00:40 -0400

    add task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- Create a table, users with attributes
 -- Table should define unique characters such as id, email, and name
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id ` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
+CREATE TABLE IF NOT EXISTS users (
+    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
