9209929cb11c1610d68ac2558e5f323ea3dcef8a
joethesaint <joebamisaye068@gmail.com>
2023-04-08 23:07:26 -0400

    add task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create a table users with a unique email and enumerated country
+-- attributes: id, email, name, and country
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
