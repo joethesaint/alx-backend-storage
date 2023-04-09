d26b0c7ef136ba83549423ca738483f48b9f4574
joethesaint <joebamisaye068@gmail.com>
2023-04-09 00:51:33 -0400

    add task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- create a function
+-- Divide and return the first by the second
+
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+  IF b = 0 THEN
+    RETURN 0;
+  ELSE 
+    RETURN (a * 1.0) / b;
+  END IF;
+END;$$
+DELIMITER ;
